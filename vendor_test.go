821571b99384d9ebf7da4066138f7876c2c32cb0
Arda Karaduman <akaraduman@gmail.com>
2016-02-26 20:00:17 +0900

    add test cases for no oiu txt

--- vendor_test.go
+++ vendor_test.go
@@ -37,3 +37,32 @@ func TestLookup(t *testing.T) {
 		t.Errorf("Error looking up dummy string")
 	}
 }
+
+func TestLookupNoInit(t *testing.T) {
+	VendorMap = make(map[string]string)
+	MacBookVendorString := Lookup(MacBookMac)
+	if MacBookVendorString != "Unknown" {
+		t.Errorf("Error looking up MacBook Mac. %v", MacBookVendorString)
+	}
+
+	OnePlusVendorString := Lookup(OnePlusOneMac)
+	if OnePlusVendorString != "Unknown" {
+		t.Errorf("Error looking up OnePlus Mac. %v", OnePlusVendorString)
+	}
+
+	if Lookup(RandomMac1) != "Random" {
+		t.Errorf("Error looking up Random Mac. 1")
+	}
+
+	if Lookup(RandomMac2) != "Random" {
+		t.Errorf("Error looking up Random Mac. 2")
+	}
+
+	if Lookup("") != "Malformed" {
+		t.Errorf("Error looking up empty string %v", Lookup(""))
+	}
+
+	if Lookup("dummy") != "Malformed" {
+		t.Errorf("Error looking up dummy string")
+	}
+}
